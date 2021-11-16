e0de9fa378eb15be3736e25830a99e8ae9c48efa
Bonny-code <ndiboniface@outlook.com>
2021-11-16 16:19:56 +0000

    Using Axios to fetch data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import axios from "axios";
 
+axios.get("http://localhost:3001/notes").then((response) => {
+  const notes = response.data;
+  ReactDOM.render(<App notes={notes} />, document.getElementById("root"));
+});
+
+/*
 const notes = [
   {
     id: 1,
@@ -22,5 +29,6 @@ const notes = [
     important: true,
   },
 ];
+*/
 
-ReactDOM.render(<App notes={notes} />, document.getElementById("root"));
+//ReactDOM.render(<App notes={notes} />, document.getElementById("root"));
