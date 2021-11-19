66b15e684e75226079a01b71015b08c33790efe5
Bonny-code <ndiboniface@outlook.com>
2021-11-19 14:25:43 +0000

    Creating a Docker image

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,12 +1,12 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/notes";
+const baseUrl = "/api/notes";
 
-const getAll2 = () => {
+const getAll = () => {
   const request = axios.get(baseUrl);
   return request.then((response) => response.data);
 };
 
-const getAll = () => {
+const getAll2 = () => {
   const request = axios.get(baseUrl);
   const nonExisting = {
     id: 10000,
